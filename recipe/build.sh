f840fa9d4e4cc8d4a71866bb1a8439db454ac25d
Isuru Fernando <isuruf@gmail.com>
2019-03-17 00:18:52 -0500

    absolute path to python

--- recipe/build.sh
+++ recipe/build.sh
@@ -34,6 +34,6 @@ cmake \
   -DVTK_OPENGL_HAS_OSMESA:BOOL=ON \
   -DOSMESA_LIBRARY=${PREFIX}/lib/libOSMesa32.so \
   -DCMAKE_RULE_MESSAGES=OFF \
-  -DPYTHON_EXECUTABLE=python \
+  -DPYTHON_EXECUTABLE=${PREFIX}/bin/python \
   ..
 make install -j${CPU_COUNT}
