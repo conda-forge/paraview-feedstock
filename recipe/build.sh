df29da7a4813f1e1a0ca0d79b086ddaa59e4aebf
Bas Nijholt <bas@nijho.lt>
2021-05-21 16:38:32 +0200

    use build_variant

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,13 @@
 export LDFLAGS=`echo "${LDFLAGS}" | sed "s|-Wl,-dead_strip_dylibs||g"`
 
 mkdir build && cd build
+
+if [[ "${build_variant}" == "paraview-egl" ]]; then
+  EXTRA="-DVTK_USE_X=OFF -DVTK_OPENGL_HAS_EGL=ON -DPARAVIEW_PLUGIN_ENABLE_pvNVIDIAIndeX=ON -DVTK_ENABLE_VISRTX=ON -DVTK_ENABLE_OSPRAY=ON"
+elif [[ "${build_variant}" == "paraview" ]]; then
+  EXTRA="-DPARAVIEW_BUILD_WITH_EXTERNAL=ON"
+fi
+
 cmake -LAH \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
   -DCMAKE_PREFIX_PATH=${PREFIX} \
@@ -16,12 +23,12 @@ cmake -LAH \
   -DPARAVIEW_USE_PYTHON=ON \
   -DPython3_FIND_STRATEGY=LOCATION \
   -DPython3_ROOT_DIR=${PREFIX} \
-  -DPARAVIEW_BUILD_WITH_EXTERNAL=ON \
   -DVTK_MODULE_USE_EXTERNAL_VTK_libharu=OFF \
   -DVTK_MODULE_USE_EXTERNAL_VTK_utf8=OFF \
   -DPARAVIEW_ENABLE_WEB=ON \
   -DPARAVIEW_ENABLE_VISITBRIDGE=ON \
   -DPARAVIEW_ENABLE_XDMF3=ON \
+  ${EXTRA} \
   ..
 make install -j${CPU_COUNT}
 
