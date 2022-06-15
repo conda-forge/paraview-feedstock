f6bae29c486f8b87d85e2c2f776680f6316d31a7
Julien Schueller <schueller@phimeca.com>
2022-06-15 12:32:04 +0200

    compile native utils

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,8 +3,6 @@
 # https://gitlab.kitware.com/paraview/paraview/issues/19645
 export LDFLAGS=`echo "${LDFLAGS}" | sed "s|-Wl,-dead_strip_dylibs||g"`
 
-mkdir build && cd build
-
 if [[ "$build_variant" == "egl" ]]; then
   CMAKE_ARGS="-DVTK_USE_X=OFF -DVTK_OPENGL_HAS_EGL=ON -DPARAVIEW_USE_QT=OFF -DVTK_MODULE_USE_EXTERNAL_VTK_glew=OFF"
   CMAKE_ARGS="${CMAKE_ARGS} -DEGL_INCLUDE_DIR=${BUILD_PREFIX}/${HOST}/sysroot/usr/include"
@@ -14,6 +12,67 @@ if [[ "$build_variant" == "egl" ]]; then
 elif [[ "$build_variant" == "qt" ]]; then
   CMAKE_ARGS=""
 fi
+
+if test "${CONDA_BUILD_CROSS_COMPILATION}" == "1"
+then
+  # ld: malformed 64-bit a.b.c.d.e version number: 9.0.20210922
+  echo "set(VTK_BUILD_VERSION 0)" >> VTK/CMake/vtkVersion.cmake
+
+#   mkdir build-native-vtk
+#   cd build-native-vtk
+#   CC=$CC_FOR_BUILD CXX=$CXX_FOR_BUILD CFLAGS= CXXFLAGS= CPPFLAGS= LDFLAGS=${LDFLAGS//$PREFIX/$BUILD_PREFIX} \
+#      cmake -DCMAKE_INSTALL_PREFIX=$SRC_DIR/vtk-compile-tools \
+#      -DCMAKE_PREFIX_PATH=$BUILD_PREFIX \
+#      -DCMAKE_INSTALL_LIBDIR=lib \
+#      -DCMAKE_BUILD_TYPE=Release \
+#      -DVTK_BUILD_COMPILE_TOOLS_ONLY=ON ../VTK
+#   make install -j${CPU_COUNT}
+#   cd ..
+  MAJ_MIN=$(echo $PKG_VERSION | rev | cut -d"." -f2- | rev)
+  CMAKE_ARGS="${CMAKE_ARGS} -DProtobuf_PROTOC_EXECUTABLE=$BUILD_PREFIX/bin/protoc"
+#   CMAKE_ARGS="${CMAKE_ARGS} -DVTKCompileTools_DIR=$SRC_DIR/vtk-compile-tools/lib/cmake/vtkcompiletools-${MAJ_MIN}/"
+#   CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_REQUIRE_LARGE_FILE_SUPPORT=1 -DCMAKE_REQUIRE_LARGE_FILE_SUPPORT__TRYRUN_OUTPUT="
+#   CMAKE_ARGS="${CMAKE_ARGS} -DVTK_REQUIRE_LARGE_FILE_SUPPORT_EXITCODE=0 -DVTK_REQUIRE_LARGE_FILE_SUPPORT_EXITCODE__TRYRUN_OUTPUT="
+#   CMAKE_ARGS="${CMAKE_ARGS} -DXDMF_REQUIRE_LARGE_FILE_SUPPORT_EXITCODE=0 -DXDMF_REQUIRE_LARGE_FILE_SUPPORT_EXITCODE__TRYRUN_OUTPUT="
+
+  mkdir build-native
+  cd build-native
+  CC=$CC_FOR_BUILD CXX=$CXX_FOR_BUILD CFLAGS= CXXFLAGS= CPPFLAGS= LDFLAGS=${LDFLAGS//$PREFIX/$BUILD_PREFIX} \
+     cmake -DCMAKE_PREFIX_PATH=$BUILD_PREFIX -DCMAKE_BUILD_TYPE=Release ..
+  make ProcessXML WrapClientServer WrapHierarchy WrapPython WrapPythonInit -j${CPU_COUNT}
+  cd ..
+  echo "add_executable(ParaView::ProcessXML IMPORTED GLOBAL)" > Utilities/ProcessXML/CMakeLists.txt
+  echo "set_property(TARGET ParaView::ProcessXML PROPERTY IMPORTED_LOCATION $PWD/build-native/bin/vtkProcessXML-pv${MAJ_MIN})" >> Utilities/ProcessXML/CMakeLists.txt
+  echo "add_custom_target(ProcessXML)" >> Utilities/ProcessXML/CMakeLists.txt
+  echo "add_dependencies(ProcessXML ParaView::ProcessXML)" >> Utilities/ProcessXML/CMakeLists.txt
+
+  echo "add_executable(ParaView::WrapClientServer IMPORTED GLOBAL)" > Utilities/WrapClientServer/CMakeLists.txt
+  echo "set_property(TARGET ParaView::WrapClientServer PROPERTY IMPORTED_LOCATION $PWD/build-native/bin/vtkWrapClientServer-pv${MAJ_MIN})" >> Utilities/WrapClientServer/CMakeLists.txt
+  echo "add_custom_target(WrapClientServer)" >> Utilities/WrapClientServer/CMakeLists.txt
+  echo "add_dependencies(WrapClientServer ParaView::WrapClientServer)" >> Utilities/WrapClientServer/CMakeLists.txt
+
+  echo "add_executable(VTK::WrapHierarchy IMPORTED GLOBAL)" > VTK/Wrapping/Tools/LocalUserOptions.cmake
+  echo "set_property(TARGET VTK::WrapHierarchy PROPERTY IMPORTED_LOCATION $PWD/build-native/bin/vtkWrapHierarchy-pv${MAJ_MIN})" >> VTK/Wrapping/Tools/LocalUserOptions.cmake
+  echo "add_custom_target(WrapHierarchy)" >> VTK/Wrapping/Tools/LocalUserOptions.cmake
+  echo "add_dependencies(WrapHierarchy VTK::WrapHierarchy)" >> VTK/Wrapping/Tools/LocalUserOptions.cmake
+
+  echo "add_executable(VTK::WrapPython IMPORTED GLOBAL)" >> VTK/Wrapping/Tools/LocalUserOptions.cmake
+  echo "set_property(TARGET VTK::WrapPython PROPERTY IMPORTED_LOCATION $PWD/build-native/bin/vtkWrapPython-pv${MAJ_MIN})" >> VTK/Wrapping/Tools/LocalUserOptions.cmake
+  echo "add_custom_target(WrapPython)" >> VTK/Wrapping/Tools/LocalUserOptions.cmake
+  echo "add_dependencies(WrapPython VTK::WrapPython)" >> VTK/Wrapping/Tools/LocalUserOptions.cmake
+
+  echo "add_executable(VTK::WrapPythonInit IMPORTED GLOBAL)" >> VTK/Wrapping/Tools/LocalUserOptions.cmake
+  echo "set_property(TARGET VTK::WrapPythonInit PROPERTY IMPORTED_LOCATION $PWD/build-native/bin/vtkWrapPythonInit-pv${MAJ_MIN})" >> VTK/Wrapping/Tools/LocalUserOptions.cmake
+  echo "add_custom_target(WrapPythonInit)" >> VTK/Wrapping/Tools/LocalUserOptions.cmake
+  echo "add_dependencies(WrapPythonInit VTK::WrapPythonInit)" >> VTK/Wrapping/Tools/LocalUserOptions.cmake
+
+  # disable plugins doc
+  $BUILD_PREFIX/bin/curl -L https://gitlab.kitware.com/paraview/paraview/-/merge_requests/5613.patch | patch -p1
+  CMAKE_ARGS="${CMAKE_ARGS} -DPARAVIEW_PLUGIN_DISABLE_XML_DOCUMENTATION=ON -DPARAVIEW_ENABLE_EMBEDDED_DOCUMENTATION=OFF"
+  CMAKE_ARGS="${CMAKE_ARGS} -Dqt_xmlpatterns_executable=$BUILD_PREFIX/bin/xmlpatterns"
+fi
+
+mkdir build && cd build
 
 cmake -LAH \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
